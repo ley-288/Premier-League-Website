d86eddc60f19f65071f7498be0e73bcfba6fedc1
Leyton Nightingale <leytonnightingale@Leytons-Air.lan>
2020-07-22 16:33:10 +0200

    first commit

---
+++ application.js
@@ -0,0 +1,5 @@
+document.getElementById("arsenal").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("arsenal").onclick = <a href = "arsenal.html"></a>;
+}
